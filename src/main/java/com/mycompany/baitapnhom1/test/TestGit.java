353b6179dcfd7b2a06505f8f742fca7411b1eaaa
kienl <kienluu61@gmail.com>
2024-03-20 07:35:45 +0700

    kien

--- src/main/java/com/mycompany/baitapnhom1/test/TestGit.java
+++ src/main/java/com/mycompany/baitapnhom1/test/TestGit.java
@@ -8,7 +8,13 @@ public class TestGit {
     private String getKienString(){
         return "this is kien";
     }
-    private String getMeiiiString(){
+
+    private String getMeiiiString() {
         return "Mei xinh dep tuyet voi";
+    }
+
+    private String getRandomString() {
+        return "this is from a random person";
+
     }
 }
